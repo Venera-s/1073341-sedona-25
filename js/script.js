69b268e6acbce1aa560cfec61ca53cada17ed6f6
venera-s <coldlives@gmail.com>
2020-02-17 23:26:31 +0300

    js modal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 var link = document.querySelector(".btn-form");
 
 var popup = document.querySelector(".modal-form");
-var close = document.querySelector(".modal-close");
 
 var form = document.querySelector("form");
-var arrival = document.querySelector("[name=arrival]");
-var departure = document.querySelector("[name=departure]");
-var adults = document.querySelector("[name=adults]");
-var children = document.querySelector("[name=childre]");
+var arrival1 = document.querySelector("[name=arrival]");
+var departure2 = document.querySelector("[name=departure]");
+var adults3 = document.querySelector("[name=adults]");
+var children4 = document.querySelector("[name=children]");
 
 
 link.addEventListener("click", function (evt) {
@@ -19,11 +18,25 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!arrival.value || !departure.value || !adults.value || !children.value) {
+  if (!arrival1.value || !departure2.value || !adults3.value || !children4.value) {
   evt.preventDefault();
-  console.log("Заполните данные");
+  popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
 }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show"))
+    {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
